5cc922b55bd5ca44c157e1bf0ecbc71d1b36874e
Erick <123erickrh@gmail.com>
2022-08-01 21:42:47 -0500

    Working on BeerItem. Normalize.css was added.

--- src/BeerItem/index.js
+++ src/BeerItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './BeerItem.css';
+import cassels from '../img/cassels.png';
 
 function BeerItem() {
   return (
@@ -7,7 +8,14 @@ function BeerItem() {
       <div className='gridItemContainer'>
 
         <li className="beerItemContainer">
-          <div className="itemContainer"></div>
+          <div className="itemContainer">
+            <div className="frame">
+              <img src={cassels} alt="cassels" />
+              <p className='beerTitle'>Cassels Milk Stou</p>
+              <p className="beerDescription">Cassels & Sons Brewing. Cerveza porter y stout</p>
+              <p className="price">$75.000</p>
+            </div>
+          </div>
           <div className="agregarButton"></div>
         </li>
 
