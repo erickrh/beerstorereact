f067461bfa6aa2f05d158ea80d278934e7c0c871
Erick <123erickrh@gmail.com>
2022-08-05 21:55:35 -0500

    Letters colors changed to gray. MoreProducts added.

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -4,6 +4,7 @@ import { Header } from '../Header';
 import { Title } from '../Title';
 import { BeerList } from '../BeerList';
 import { BeerItem } from '../BeerItem';
+import { MoreProducts } from '../MoreProducts';
 
 function AppUI() {
   const {
@@ -15,24 +16,27 @@ function AppUI() {
   
   return (
     <React.Fragment>
+      
       <Header />
 
       <Title />
 
       <BeerList>
         {isLoaded &&
-          items.products.map((item) => (
-            <BeerItem
-              key={item.name}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              img={item.img}
-              filterId={item.filterId}
-            />
-          ))
+        items.products.map((item) => (
+          <BeerItem
+            key={item.name}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            img={item.img}
+            filterId={item.filterId}
+          />
+        ))
         }
       </BeerList>
+
+      <MoreProducts />
 
     </React.Fragment>
   );
